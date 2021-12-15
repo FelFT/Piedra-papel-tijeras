d8d7fed1d1b10616b5f55d885eca6c0bc7439cc7
FelFT <felipe_1357@outlook.com>
2021-12-15 17:00:02 -0600

    Files 1 and 2 completed

--- 1-imagenes.py
+++ 1-imagenes.py
@@ -3,55 +3,54 @@ import os
 import imutils
 import time
 
-mano = 'piedra'
-#mano = 'papel'
-#mano = 'tijeras'
-#mano = 'arriba'
-#mano = 'abajo'
+mano = ['piedra', 'papel', 'tijeras', 'arriba','abajo', 'nada']
+directory = os.getcwd()
+os.makedirs(directory + '/imgs', exist_ok=True)
+path = directory + '/imgs'
 
-dataPath = r'C:\Users\HP\Desktop\Machine Learning\piedra_papel_tijeras'
-
-Path = dataPath + '/' + mano
-
-if not os.path.exists(Path):
-  print('Carpeta creada: ',Path)
-  os.makedirs(Path)
+for m in mano:
+  os.makedirs(path + '/' + m, exist_ok=True)
 
 cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
 
-i = 0
+for m in mano:
+  i = 0
+  while True:
 
-while True:
-
-  ret, frame = cap.read()
-  if ret == False:
+    ret, frame = cap.read()
+    if ret == False:
            break
         
-  frame =  imutils.resize(frame, width=640)
-  gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-  auxFrame = frame.copy()
-  cv2.rectangle(frame, (50,100),(250,300),(0,255,0),2)
-  roi = auxFrame[100:300,50:250]
+    frame =  imutils.resize(frame, width=640)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    auxFrame = frame.copy()
+    cv2.rectangle(frame, (50,100),(250,300),(0,255,0),2)
+    roi = auxFrame[100:300,50:250]
 
-  font = cv2.FONT_HERSHEY_SIMPLEX
-  
-  cv2.putText(frame, "Collecting {}".format(i),(5, 50), font, 0.7, (0, 255, 255), 2, cv2.LINE_AA)
-  
-  if cv2.waitKey(1) & 0xFF == ord('q'):
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    font1 = cv2.FONT_ITALIC
+
+    cv2.putText(frame, "Capturando {} {}".format(m, i),(5, 50), font, 0.7, (0, 255, 255), 2, cv2.LINE_AA)
+    cv2.putText(frame, "Presione n para cambiar posicion a capturar",(5, 380), font1, 0.5, (187, 47, 0), 1, cv2.LINE_AA)
+    cv2.putText(frame, "Presione q para capturar la imagen",(5, 400), font1, 0.5, (187, 47, 0), 1, cv2.LINE_AA)
+    cv2.putText(frame, "Presione e para salir",(5, 420), font1, 0.5, (187, 47, 0), 1, cv2.LINE_AA)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         
-    roi = cv2.resize(roi,(150,150),interpolation=cv2.INTER_CUBIC)
-    cv2.imwrite(Path + '/img_{}.jpg'.format(i),roi)
+      roi = cv2.resize(roi,(180,180),interpolation=cv2.INTER_CUBIC)
+      cv2.imwrite(path+'/'+ m + '/img_{}.jpg'.format(i),roi)
 
-    i+=1
+      i+=1
+                
+    cv2.imshow('Captura de imagenes',frame)
+
+    if (cv2.waitKey(1) & 0xFF == ord('n')) or (i==300):
+      break
+
+    if (cv2.waitKey(1) & 0xFF == ord('e')):
+      cap.release()
+      cv2.destroyAllWindows()
+  time.sleep(2)
     
-
-        
-            
-  cv2.imshow('roi',roi)
-  cv2.imshow('frame',frame)
-
-  if (cv2.waitKey(1) & 0xFF == ord('e')) or (i==200):
-    break
-
 cap.release()
 cv2.destroyAllWindows()
--- 2-train.py
+++ 2-train.py
@@ -3,11 +3,37 @@ import numpy as np
 import cv2
 import os
 import PIL
-
-
 import pathlib
 
-data_dir = r'C:\Users\HP\Desktop\Machine Learning\piedra_papel_tijeras'
+from sklearn.model_selection import train_test_split
+from keras.utils import np_utils
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.preprocessing.image import ImageDataGenerator
+
+
+def modelo_CNN():
+    model = Sequential()
+    model.add(Conv2D(16, (3,3), input_shape=(180, 180, 3), activation='relu'))
+    model.add(MaxPooling2D(2,2))
+    model.add(Dropout(0.2))
+    model.add(Conv2D(32, (3,3), activation='relu'))
+    model.add(MaxPooling2D(2,2))
+    model.add(Dropout(0.2))
+    model.add(Conv2D(64, (3,3), activation='relu'))
+    model.add(MaxPooling2D(2,2))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dense(num_classes, activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
+
+data_dir = os.getcwd()+'/imgs'
 data_dir = pathlib.Path(data_dir)
 
 image_count = len(list(data_dir.glob('*/*.jpg')))
@@ -42,14 +68,10 @@ for mano_name, images in mano_dict.items():
 X = np.array(X)
 y = np.array(y)
 
-from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
 
 X_train_scaled = X_train / 255
 X_test_scaled = X_test / 255
-
-# one hot encode outputs
-from keras.utils import np_utils
 
 y_train = np_utils.to_categorical( y_train )
 y_test = np_utils.to_categorical( y_test )
@@ -58,8 +80,6 @@ num_classes = y_test.shape[1]
 num_classes
 
 
-#Aumento de imagen
-from keras.preprocessing.image import ImageDataGenerator
 aug = ImageDataGenerator(rotation_range=30, width_shift_range=0.1,
 	height_shift_range=0.1, shear_range=0.2, zoom_range=0.2,
 	horizontal_flip=True, fill_mode="nearest")
@@ -71,30 +91,6 @@ datagen = ImageDataGenerator(rotation_range=30,
                          horizontal_flip=True, 
                          fill_mode="nearest")
 
-from keras.datasets import mnist
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import Dropout
-from keras.layers import Flatten
-from keras.layers.convolutional import Conv2D
-from keras.layers.convolutional import MaxPooling2D
-
-def modelo_CNN():
-    model = Sequential()
-    model.add(Conv2D(16, (3,3), input_shape=(180, 180, 3), activation='relu'))
-    model.add(MaxPooling2D(2,2))
-    model.add(Dropout(0.2))
-    model.add(Conv2D(32, (3,3), activation='relu'))
-    model.add(MaxPooling2D(2,2))
-    model.add(Dropout(0.2))
-    model.add(Conv2D(64, (3,3), activation='relu'))
-    model.add(MaxPooling2D(2,2))
-    model.add(Flatten())
-    model.add(Dense(128, activation='relu'))
-    model.add(Dense(num_classes, activation='softmax'))
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    return model
-
 model =modelo_CNN()
 
 history1 = model.fit_generator(datagen.flow(X_train_scaled, y_train, batch_size=32),
@@ -102,5 +98,4 @@ history1 = model.fit_generator(datagen.flow(X_train_scaled, y_train, batch_size=
 	epochs=10, verbose=1)
 
 model.save('model.h5')
-
 
